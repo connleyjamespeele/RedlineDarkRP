dca3184a1944356bdd788c5c844bc09a0c8bb044
connleyjamespeele <peeleconnley@gmail.com>
2026-05-08 16:41:45 +0000

    more income change

--- entities/entities/test_entity2/server.lua
+++ entities/entities/test_entity2/server.lua
@@ -5,31 +5,9 @@ function ENT:Initialize()
     DarkRP.ValidatedPhysicsInit(self, SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
-
-    self:SetMoney(0)
-    self:SetDelayBeforeIncome(30) 
-    self:SetMoneyGiveAmount(0) -- Set the default money give amount to 0
 end
 
-function ENT:Use(activator, caller)
-    print("Entity used by: " .. tostring(activator))
-    print("MoneyGiveAmount: " .. tostring(self:GetMoneyGiveAmount()))
-    print("caller: " .. tostring(caller))
-
-    if CLIENT then return end
-    if not IsValid(activator) or not activator:IsPlayer() then return end
-
-    local moneyAmount = self:GetMoney()
-    if moneyAmount > 0 then
-        self:SetMoney(0)
-        activator:addMoney(moneyAmount)
-        DarkRP.notify(activator, 0, 4, "You received $" .. moneyAmount .. " from the money box!")
-    else
-        DarkRP.notify(activator, 1, 4, "This money box is empty.")
-    end
-end
-
-function ENT:Think()
+--[[function ENT:Think()
     timer.Create("MoneyBoxIncome_" .. self:EntIndex(), self:GetDelayBeforeIncome(), 0, function()
         if not IsValid(self) then return end
         local CheckAround = ents.FindInSphere(self:GetPos(), 45)
@@ -45,4 +23,4 @@ function ENT:Think()
 
         self:SetMoney(self:GetMoney() + Income) 
     end)
-end 
+end]]
---
+++ entities/entities/test_entity2/shared.lua
@@ -0,0 +1,6 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Test Entity 2"
+ENT.Author = "Carlos"
+
+ENT.Spawnable = true
