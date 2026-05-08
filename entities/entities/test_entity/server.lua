f59eeb6a9e29d0a004b34ba07de3c0eaf556848d
connleyjamespeele <peeleconnley@gmail.com>
2026-05-08 14:40:42 +0000

    test enity

--- entities/entities/test_entity/server.lua
+++ entities/entities/test_entity/server.lua
@@ -6,5 +6,19 @@ function ENT:Initialize()
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
 
-    
+    self:SetMoneyGiveAmount(100) -- Set the default money give amount to 100
+end
+
+function ENT:Use(activator, caller)
+    if CLIENT then return end
+    if not IsValid(activator) or not activator:IsPlayer() then return end
+
+    local moneyAmount = self:GetMoneyGiveAmount()
+    if moneyAmount > 0 then
+        self:SetMoneyGiveAmount(0)
+        activator:addMoney(moneyAmount)
+        DarkRP.notify(activator, 0, 4, "You received $" .. moneyAmount .. " from the money box!")
+    else
+        DarkRP.notify(activator, 1, 4, "This money box is empty.")
+    end
 end
